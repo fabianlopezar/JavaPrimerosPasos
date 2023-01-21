4f92916540e89a070d80a6fbf29d27917910f909
fabian lopez <fabianlopez928@gmail.com>
2023-01-21 16:20:50 -0500

    primeros pasos

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,27 @@
+
+package hola.mundo;
+
+public class HolaMundo {
+
+  
+    public static void main(String[] args) {
+   //Aca debe ir el codigo a ejecutarse
+        System.out.println("Hola Mundo!");
+        System.out.println("Soy Fabian");
+        int numero =5;
+        String nombre="fabian";
+        char letra = 'f';
+        double temperatura=5.5;
+        boolean siOno=false;
+        long largo=1234569;
+        
+     int num1, num2,resultado;
+     
+     num1=4;
+     num2=2;
+     
+     resultado = num1+num2;
+        System.out.println("El resultado de la suma es de: "+ resultado);
+    }
+    
+}
